f44aaa2d0d83ab58e07036810d72c8edf816f870
Iurii Ovsiannikov <jgoldste@z2t10c3.42bangkok.com>
2023-08-27 18:29:56 +0700

    parser_Aug27_18-29

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 19:17:22 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/08/27 18:12:53 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/08/27 18:21:12 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,31 @@ int	main(int argc, char **argv)
 {
 	t_data	*data;
 
-	argc = 2;
-	argv[1] = ft_strdup("maps/subject_map.cub");
+	// argc = 2;
+	// argv[1] = ft_strdup("maps/subject_map.cub");
 
 	data = parse(argc, argv);
 	if (!data)
 		return (1);
 
-	if (data)
-	{
-		if (data->map_file)
-		{
-			if (data->map_file->content)
-			{
-				printf("Map check: started\n");
-				for (int i = 0; data->map_file->content[i]; i++)
-					printf("%s", data->map_file->content[i]);
-				printf("Map check: finished\n");
-			}
-		}
-	}
+	// if (data)
+	// {
+	// 	if (data->map_file)
+	// 	{
+	// 		if (data->map_file->content)
+	// 		{
+	// 			printf("Map check: started\n");
+	// 			for (int i = 0; data->map_file->content[i]; i++)
+	// 				printf("%s", data->map_file->content[i]);
+	// 			printf("Map check: finished\n");
+	// 		}
+	// 	}
+	// }
+
 	check_game_dir();
-	free(argv[1]);
-	argv[1] = NULL;
+
+	// free(argv[1]);
+	// argv[1] = NULL;
 
 	data = free_data(data);
 	return (0);
--- src/parser/parse_map.c
+++ src/parser/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 14:33:22 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/08/27 18:03:02 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/08/27 18:21:22 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,19 @@
 
 int	parse_map(t_data *data)
 {
+	if (data)
+	{
+		if (data->map_file)
+		{
+			if (data->map_file->content)
+			{
+				printf("Map check: started\n");
+				for (int i = 0; data->map_file->content[i]; i++)
+					printf("%s", data->map_file->content[i]);
+				printf("Map check: finished\n");
+			}
+		}
+	}
 	free_data_file(data);
 	return (0);
 }
