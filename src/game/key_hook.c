c99da876f1504ce183c85e4513d8c4cf12abee4f
Iurii Ovsiannikov <jgoldste@z1t1c2.42bangkok.com>
2023-09-14 16:49:46 +0700

    game_Sep14_16-49

--- src/game/key_hook.c
+++ src/game/key_hook.c
@@ -16,5 +16,18 @@ int	key_hook(int keycode, t_game *game)
 {
 	if (keycode == ESCAPE)
 		close_game_exit(game);
+	if (keycode == 13)
+	{
+		// change angle
+		printf("W pressed");
+		game->rayAngle++;
+		mlx_clear_window(game->mlx_data->mlx_ptr, game->mlx_data->win_ptr);
+		raycast(game);
+		// clear ??
+		// raycast
+		// if (game->map[game->y - 1][game->x] != '1')
+		// 	game->y -= 1;
+
+	}
 	return (0);
 }
