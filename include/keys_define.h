6b08a60162265e2bd5460d74c2a33664b132dde2
Dmitrii Larionov <dlariono@z1t1c1.42bangkok.com>
2023-09-16 13:04:51 +0700

    ходим сквозь стены

--- include/keys_define.h
+++ include/keys_define.h
@@ -16,5 +16,7 @@
 # define ESCAPE 53
 # define LEFT 123
 # define RIGHT 124
+# define UP 126
+# define DOWN 125
 
 #endif
--- src/game/key_hook.c
+++ src/game/key_hook.c
@@ -12,21 +12,41 @@
 
 #include "cub3d.h"
 
+
+void move_player(t_game *game, int direction)
+{
+	double radians;
+	double	step_size;
+
+	step_size = 0.2;
+	radians = degree_to_radians(game->map->player_orient);
+	game->map->player_x += cos(radians) * direction * step_size;
+	game->map->player_y += sin(radians) * direction * step_size;
+}
+
 int	key_hook(int keycode, t_game *game)
 {
 	if (keycode == ESCAPE)
 		close_game_exit(game);
-	else if (keycode == RIGHT)
+	else if (keycode == RIGHT || keycode == 2)
 	{
 		game->map->player_orient += game->values->inc_angle * TURN_SPEED;
 		if (game->map->player_orient > 360)
 			game->map->player_orient -= 360;
 	}
-	else if (keycode == LEFT)
+	else if (keycode == LEFT || keycode == 0)
 	{
 		game->map->player_orient -= game->values->inc_angle * TURN_SPEED;
 		if (game->map->player_orient < 0)
 			game->map->player_orient += 360;
+	}
+	else if (keycode == UP || keycode == 13)
+	{
+		move_player(game, 1);
+	}
+	else if (keycode == DOWN || keycode == 1)
+	{
+		move_player(game, -1);
 	}
 	// значения направления камеры в градусах я думаю использовать для определения
 	// какую текстуру накладывать, поэтому это значение должно быть в диапазоне
@@ -43,3 +63,4 @@ int	key_hook(int keycode, t_game *game)
 	// то есть тут нужно только меня значения переменных расположения на карте
 	return (0);
 }
+
