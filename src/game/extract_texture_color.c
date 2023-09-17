2d0316b896385b5116d228b11dde75352430e41c
Iurii Ovsiannikov <jgoldste@z1t1c2.42bangkok.com>
2023-09-17 16:57:08 +0700

    jgoldste_Sep17_16-57

--- src/game/extract_texture_color.c
+++ src/game/extract_texture_color.c
@@ -21,11 +21,15 @@ int	extract_texture_color(t_game *game)
 	int angle;
 
 	angle = game->ray_angle + game->values->half_fov;
-	if (angle > 0 && angle < 90)
+	if (angle > 360)
+		angle -= 360;
+	else if (angle < 0)
+		angle += 360;
+	if (angle >= 0 && angle <= 90)
 		return (north);	
 	else if (angle > 90 && angle < 180)
 		return(east);
-	else if (angle > 180 && angle < 270)
+	else if (angle >= 180 && angle <= 270)
 		return (south);
 	else if (angle > 270 && angle < 360)
 		return (west);
