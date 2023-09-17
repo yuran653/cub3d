cc8f3550fe4608df9b50329a7d3a7e5759c24c7b
Iurii Ovsiannikov <jgoldste@z1t1c2.42bangkok.com>
2023-09-17 16:51:49 +0700

    jgoldste_Sep17_16-51

--- src/game/extract_texture_color.c
+++ src/game/extract_texture_color.c
@@ -14,10 +14,20 @@
 
 int	extract_texture_color(t_game *game)
 {
-	// int north = 0x9999ff;
-	// int south = 0xff9999;
-	// int east = 0xffff99;
-	// int west = 0x99ffff;
-	
+	int north = 0x9999ff;
+	int south = 0xff9999;
+	int east = 0xffff99;
+	int west = 0x99ffff;
+	int angle;
+
+	angle = game->ray_angle + game->values->half_fov;
+	if (angle > 0 && angle < 90)
+		return (north);	
+	else if (angle > 90 && angle < 180)
+		return(east);
+	else if (angle > 180 && angle < 270)
+		return (south);
+	else if (angle > 270 && angle < 360)
+		return (west);
 	return (0x0);
 }
