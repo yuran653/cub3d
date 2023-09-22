617c310a33734c77970f92571d41c61cb45dd9a0
Iurii Ovsiannikov <jgoldste@z1t1c2.42bangkok.com>
2023-09-22 18:53:39 +0700

    jgoldste_textures_are_defined__Sep22_18-53

--- src/game/define_texture_side.c
+++ src/game/define_texture_side.c
@@ -6,53 +6,35 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 14:58:56 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/09/22 18:15:54 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/09/22 18:47:57 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	define_texture_side(t_game *game)
+static int	north_south(int prev_y, int y)
 {
-	if (game->prev_y == game->y)
-	{
-		if (game->prev_x < game->x)
-			return (T_WEST);
-		if (game->prev_x > game->x)
-			return (T_EAST);
-	}
-	else if (game->prev_x == game->x)
-	{
-		if (game->prev_y < game->y)
-			return (T_SOUTH);
-		if (game->prev_y > game->y)
-			return (T_NORTH);
-
-	}
+	if (prev_y <= y)
+		return (T_SOUTH);
+	if (prev_y >= y)
+		return (T_NORTH);
 	return (-1);
 }
 
-// int	define_texture_side(t_game *game)
-// {
-// 	int north = 0x9999ff;
-// 	int south = 0xff9999;
-// 	int east = 0xffff99;
-// 	int west = 0x99ff99;
+static int	east_west(int prev_x, int x)
+{
+	if (prev_x <= x)
+			return (T_WEST);
+	if (prev_x >= x)
+		return (T_EAST);
+	return (-1);
+}
 
-// 	if (game->prev_y == game->y)
-// 	{
-// 		if (game->prev_x < game->x)
-// 			return (west);
-// 		if (game->prev_x > game->x)
-// 			return (east);
-// 	}
-// 	else if (game->prev_x == game->x)
-// 	{
-// 		if (game->prev_y < game->y)
-// 			return (south);
-// 		if (game->prev_y > game->y)
-// 			return (north);
-
-// 	}
-// 	return (0X0);
-// }
+int	define_texture_side(t_game *game)
+{
+	if (game->prev_y == game->y)
+		return (east_west(game->prev_x, game->x));
+	else
+		return (north_south(game->prev_y, game->y));
+	return (-1);
+}
