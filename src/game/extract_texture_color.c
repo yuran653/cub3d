49f78cb5cce362a7d05cdd7d3b1b8fcd24bbc7fa
Dmitrii Larionov <mikrolux@gmail.com>
2023-09-22 18:58:58 +0700

    text_side_fix

--- src/game/extract_texture_color.c
+++ src/game/extract_texture_color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   extract_texture_color.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
+/*   By: dmitrylarionov <dmitrylarionov@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 15:18:50 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/09/22 18:19:18 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/09/22 18:58:13 by dmitrylario      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,6 @@
 
 int	extract_texture_color(t_game *game, double y_pos_fixed, int texture)
 {
-	// int north = 0x9999ff;
-	// int south = 0xff9999;
-	// int east = 0xffff99;
-	// int west = 0x99ffff;
-	// int angle;
-	// int img_pixel;
-	// int x = 10;
-	// int y = 5;
-
- 	// img_pixel= ((int *)game->map->texture[0].addr)[y * game->map->texture[0].width + x];
-
-	// int red = (img_pixel >> 16) & 0xFF;
-	// int green = (img_pixel >> 8) & 0xFF;
-	// int blue = img_pixel & 0xFF;
-	// // подменяем цветом с позиции x,y текстуры
-	// west = (red << 16) | (green << 8) | blue;
-
-	// // printf("test color %x\n", north);
-	// // printf("test color %x\n", test_color);
-
-	// angle = game->ray_angle + game->values->half_fov;
-	// if (angle > 360)
-	// 	angle -= 360;
-	// else if (angle < 0)
-	// 	angle += 360;
-	// if (angle >= 0 && angle <= 90)
-	// 	return (north);
-	// else if (angle > 90 && angle < 180)
-	// 	return(east);
-	// else if (angle >= 180 && angle <= 270)
-	// 	return (south);
-	// else if (angle > 270 && angle < 360)
-	// 	return (west);
-	// return (0x0);
-
 	if (texture == -1)
 		return (0X0);
 
@@ -58,8 +23,10 @@ int	extract_texture_color(t_game *game, double y_pos_fixed, int texture)
 	// int r;
 	// int g;
 	// int b;
-
-	x_pos = (int)((game->ray_x - floor(game->ray_x)) * (double)game->map->texture[texture].width); // 0 - 100 позиция в тек квадрате взятая с дабл, сменить в зав от X, Y куда смотрим
+	if (texture == 0 || texture == 1)
+		x_pos = (int)((game->ray_x - floor(game->ray_x)) * (double)game->map->texture[texture].width); // 0 - 100 позиция в тек квадрате взятая с дабл, сменить в зав от X, Y куда смотрим
+	else
+		x_pos = (int)((game->ray_y - floor(game->ray_y)) * (double)game->map->texture[texture].width); // 0 - 100 позиция в тек квадрате взятая с дабл, сменить в зав от X, Y куда смотрим
 	y_pos_fixed = (double)game->map->texture[texture].width * y_pos_fixed; // коорд Y в текстуре например 155
  	img_pixel = ((int *)game->map->texture[texture].addr)[(int)y_pos_fixed * game->map->texture[texture].width + x_pos ];
 	// r = (img_pixel >> 16) & 0xFF;
